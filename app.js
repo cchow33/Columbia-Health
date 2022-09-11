4613ca5efbf74a5a36f50938266565be1ff56d6b
cchow33 <109078860+cchow33@users.noreply.github.com>
2022-09-11 01:14:22 -0400

    Scroll up button

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const navList = document.getElementById('nav-list');
 toggleButton.addEventListener('click', () => {
     navList.classList.toggle('active');
 })
+
+
+// Scroll to the top button
+let scroll = document.getElementById("scroll-btn");
+
+// Display button when screen scrolled 30px from top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    scroll.style.display = "block";
+  } else {
+    scroll.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
+}
